af9fcde7918b6bd449961b33d0525541523a64f7
ModyQyW <wurui-dev@foxmail.com>
2022-12-29 10:47:17 +0800

    test: update utils test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -2,40 +2,39 @@ import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { describe, it, expect } from 'vitest';
 import { getFileDir, getFileField, getFilePath } from './utils';
-import { defaultConfig } from './config';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 describe('utils', () => {
   it('getFileField', () => {
     expect(
-      getFileField(defaultConfig, [
+      getFileField([
         { entry: [__dirname, 'fixtures', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
         { entry: [__dirname, '**', 'fixtures', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
       ]),
-    ).toBe('touristappid');
-    expect(
-      getFileField(defaultConfig, [{ entry: ['not-exist.file'], prop: ['not', 'exist', 'prop'] }]),
-    ).toBe('');
+    ).toBe('manifest.json appid');
+    expect(getFileField([{ entry: ['not-exist.file'], prop: ['not', 'exist', 'prop'] }])).toBe(
+      undefined,
+    );
   });
 
   it('getFilePath', () => {
     expect(
-      getFilePath(defaultConfig, [
+      getFilePath([
         { entry: [__dirname, 'fixtures', 'manifest.json'] },
         { entry: [__dirname, '**', 'fixtures', 'manifest.json'] },
       ]),
     ).toBe(resolve(__dirname, 'fixtures', 'manifest.json'));
-    expect(getFilePath(defaultConfig, [{ entry: ['not-exist.file'] }])).toBe('');
+    expect(getFilePath([{ entry: ['not-exist.file'] }])).toBe(undefined);
   });
 
   it('getFileDir', () => {
     expect(
-      getFileDir(defaultConfig, [
+      getFileDir([
         { entry: [__dirname, 'fixtures', 'manifest.json'] },
         { entry: [__dirname, '**', 'fixtures', 'manifest.json'] },
       ]),
     ).toBe(resolve(__dirname, 'fixtures'));
-    expect(getFileDir(defaultConfig, [{ entry: ['not-exist.file'] }])).toBe('');
+    expect(getFileDir([{ entry: ['not-exist.file'] }])).toBe(undefined);
   });
 });
