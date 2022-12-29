fd3a36d408d6fb134a8fef35fc5e8f08bcc22b43
ModyQyW <wurui-dev@foxmail.com>
2022-12-29 11:02:18 +0800

    feat: update cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,28 +1,19 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import pkg from '../package.json';
-import { defaultConfig, loadConfig, mergeConfig } from './config';
+import { loadConfig } from './config';
 import { imNotifyPreview, imNotifyUpload } from './im';
 import { platformPreview, platformUpload } from './platform';
 import { logger, validatePlatforms, validateIms } from './utils';
-import type { UniDeployConfig, Im, Platform } from './types';
+import type { Im, Platform } from './types';
 
 const program = new Command(pkg.name).version(pkg.version).description(pkg.description);
-
-async function getConfig(): Promise<UniDeployConfig> {
-  try {
-    const { data } = await loadConfig();
-    return data ? mergeConfig(data) : defaultConfig;
-  } catch {
-    return defaultConfig;
-  }
-}
 
 program
   .command('validate')
   .description('检查配置文件')
   .action(async () => {
-    const config = await getConfig();
+    const config = await loadConfig();
     validatePlatforms(config);
     validateIms(config);
     // 结束
@@ -33,7 +24,7 @@ program
   .command('upload')
   .description('上传')
   .action(async () => {
-    const config = await getConfig();
+    const config = await loadConfig();
     // 检查
     const validatePlatformsResults = validatePlatforms(config);
     const validateImsResults = validateIms(config);
@@ -63,7 +54,7 @@ program
   .command('preview')
   .description('预览')
   .action(async () => {
-    const config = await getConfig();
+    const config = await loadConfig();
     // 检查
     const validatePlatformsResults = validatePlatforms(config);
     const validateImsResults = validateIms(config);
