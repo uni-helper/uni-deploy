ada72e68be7beddf6d663521ff40cc3d96bcb238
ModyQyW <wurui-dev@foxmail.com>
2022-12-29 10:58:44 +0800

    feat: update platforms

--- src/platform/mp-alipay.ts
+++ src/platform/mp-alipay.ts
@@ -1,72 +1,51 @@
 import * as _mindev from 'minidev';
 import pRetry from 'p-retry';
 import { PRetryOptions, UniDeployConfig } from 'src/types';
-import { getFileField, getFileDir, logger } from '../utils';
+import { logger } from '../utils';
 
 const { minidev, useDefaults } = _mindev;
 
-export const mpAlipayGetConfig = (config: UniDeployConfig) => config?.['mp-alipay'];
-
-export const mpAlipayGetProjectAppid = (config: UniDeployConfig) =>
-  (config?.['mp-alipay']?.project?.appid ??
-    getFileField(config, [
-      { entry: ['dist', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
-      { entry: ['dist', 'build', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
-      { entry: ['dist', '**', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
-    ])) as string;
-
-export const mpAlipayGetProjectPath = (config: UniDeployConfig) =>
-  config?.['mp-alipay']?.project?.projectPath ??
-  getFileDir(config, [
-    { entry: ['dist', 'mp-alipay', 'mini.project.json'] },
-    { entry: ['dist', 'build', 'mp-alipay', 'mini.project.json'] },
-    { entry: ['dist', '**', 'mp-alipay', 'mini.project.json'] },
-  ]);
-
-export const mpAlipayGetProjectPrivateKey = (config: UniDeployConfig) =>
-  config?.['mp-alipay']?.project?.privateKey ??
-  getFileField(config, [
-    { entry: ['~', '.minidev', 'config.json'], prop: ['alipay', 'authentication', 'privateKey'] },
-    { entry: ['~', '.minidev', 'config.json'], prop: ['authentication', 'privateKey'] },
-  ]);
-
-export const mpAlipayGetProjectToolId = (config: UniDeployConfig) =>
-  config?.['mp-alipay']?.project?.toolId ??
-  getFileField(config, [
-    { entry: ['~', '.minidev', 'config.json'], prop: ['alipay', 'authentication', 'toolId'] },
-    { entry: ['~', '.minidev', 'config.json'], prop: ['authentication', 'toolId'] },
-  ]);
-
 export const mpAlipayValidate = (config: UniDeployConfig) => {
-  const mpAlipayConfig = mpAlipayGetConfig(config);
-  if (!mpAlipayConfig) {
-    logger.info('没有配置支付宝小程序，跳过支付宝小程序操作。');
-    return false;
-  }
-  const appid = mpAlipayGetProjectAppid(config);
+  let isValid = true;
+  /* appid */
+  const appid = config?.['mp-alipay']?.appid;
   if (!appid) {
-    logger.info('没有配置支付宝小程序 appid，跳过支付宝小程序操作。');
-    return false;
+    logger.info('【支付宝小程序】缺少 appid');
+    isValid = false;
   }
-  const privateKey = mpAlipayGetProjectPrivateKey(config);
+  /* projectPath */
+  const projectPath = config?.['mp-alipay']?.projectPath;
+  if (!projectPath) {
+    logger.warn('【支付宝小程序】缺少 projectPath');
+    isValid = false;
+  }
+  /* privateKey */
+  const privateKey = config?.['mp-alipay']?.privateKey;
   if (!privateKey) {
-    logger.info('没有配置支付宝小程序 privateKey，跳过支付宝小程序操作。');
-    return false;
+    logger.warn('【支付宝小程序】缺少 privateKey');
+    isValid = false;
   }
-  const toolId = mpAlipayGetProjectToolId(config);
+  /* toolId */
+  const toolId = config?.['mp-alipay']?.toolId;
   if (!toolId) {
-    logger.info('没有配置支付宝小程序 toolId，跳过支付宝小程序操作。');
-    return false;
+    logger.warn('【支付宝小程序】缺少 toolId');
+    isValid = false;
   }
-  return true;
+  /* version */
+  const version = config?.['mp-alipay']?.version;
+  if (!version) {
+    logger.warn('【支付宝小程序】缺少 version');
+    isValid = false;
+  }
+  return isValid;
 };
 
 export const mpAlipayGetProject = (config: UniDeployConfig) =>
   useDefaults({
     config: {
       defaults: {
-        'alipay.authentication.privateKey': mpAlipayGetProjectPrivateKey(config),
-        'alipay.authentication.toolId': mpAlipayGetProjectToolId(config),
+        'alipay.authentication.privateKey': config?.['mp-alipay']?.privateKey,
+        'alipay.authentication.toolId': config?.['mp-alipay']?.toolId,
       },
     },
   });
@@ -76,9 +55,9 @@ export const mpAlipayUpload = async (config: UniDeployConfig, pRetryOptions?: PR
   return pRetry(
     () =>
       minidev.upload({
-        ...config?.['mp-alipay']?.upload,
-        appId: mpAlipayGetProjectAppid(config),
-        project: mpAlipayGetProjectPath(config),
+        ...config?.['mp-alipay'],
+        appId: config?.['mp-alipay']?.appid as string,
+        project: config?.['mp-alipay']?.projectPath,
       }),
     pRetryOptions,
   );
@@ -89,9 +68,9 @@ export const mpAlipayPreview = async (config: UniDeployConfig, pRetryOptions?: P
   return pRetry(
     () =>
       minidev.preview({
-        ...config?.['mp-alipay']?.preview,
-        appId: mpAlipayGetProjectAppid(config),
-        project: mpAlipayGetProjectPath(config),
+        ...config?.['mp-alipay'],
+        appId: config?.['mp-alipay']?.appid as string,
+        project: config?.['mp-alipay']?.projectPath,
       }),
     pRetryOptions,
   );
--- src/platform/mp-weixin.ts
+++ src/platform/mp-weixin.ts
@@ -1,134 +1,67 @@
+import { existsSync } from 'fs';
 import ci from 'miniprogram-ci';
 import pRetry from 'p-retry';
-import { MiniProgramCI } from 'miniprogram-ci/dist/@types/types';
-import { getFileField, getFileDir, getFilePath, logger } from '../utils';
+import { ICreateProjectOptions as WechatCreateProjectOptions } from 'miniprogram-ci/dist/@types/ci/project';
+import { IInnerUploadOptions as WechatUploadOptions } from 'miniprogram-ci/dist/@types/ci/upload';
+import { logger } from '../utils';
 import type { UniDeployConfig, PRetryOptions } from '../types';
 import type { IInnerUploadResult as WechatUploadResult } from 'miniprogram-ci/dist/@types/ci/upload';
 
-export const mpWeixinGetConfig = (config: UniDeployConfig) => config?.['mp-weixin'];
-
-export const mpWeixinGetProjectAppid = (config: UniDeployConfig) =>
-  (config?.['mp-weixin']?.project?.appid ??
-    getFileField(config, [
-      { entry: ['dist', 'mp-weixin', 'project.config.json'], prop: 'appid' },
-      { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'], prop: 'appid' },
-      { entry: ['dist', '**', 'mp-weixin', 'project.config.json'], prop: 'appid' },
-    ])) as string;
-
-export const mpWeixinGetProjectPath = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.project?.projectPath ??
-  getFileDir(config, [
-    { entry: ['dist', 'mp-weixin', 'project.config.json'] },
-    { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'] },
-    { entry: ['dist', '**', 'mp-weixin', 'project.config.json'] },
-  ]);
-
-export const mpWeixinGetProjectPrivateKeyPath = (config: UniDeployConfig) => {
-  const appid = mpWeixinGetProjectAppid(config);
-  return (
-    config?.['mp-weixin']?.project?.privateKeyPath ??
-    getFilePath(config, [
-      { entry: ['**', `private.${appid}.key`] },
-      { entry: ['**', `weixin.${appid}.key`] },
-      { entry: ['**', `wechat.${appid}.key`] },
-    ])
-  );
-};
-
-export const mpWeixinGetProjectType = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.project?.type ?? 'miniProgram';
-
 export const mpWeixinValidate = (config: UniDeployConfig) => {
-  const mpWeixinConfig = mpWeixinGetConfig(config);
-  if (!mpWeixinConfig) {
-    logger.info('没有配置微信小程序，跳过微信小程序操作。');
-    return false;
-  }
-  const appid = mpWeixinGetProjectAppid(config);
+  let isValid = true;
+  /* appid */
+  const appid = config['mp-weixin']?.appid;
   if (!appid) {
-    logger.info('没有配置微信小程序 appid，跳过微信小程序操作。');
-    return false;
+    logger.warn('【微信小程序】缺少 appid');
+    isValid = false;
   }
-  return true;
+  /* projectPath */
+  const projectPath = config?.['mp-weixin']?.projectPath;
+  if (!projectPath) {
+    logger.warn('【微信小程序】缺少 projectPath');
+    isValid = false;
+  }
+  /* privateKey & privateKeyPath */
+  const privateKey = config?.['mp-weixin']?.privateKey;
+  const privateKeyPath = config?.['mp-weixin']?.privateKeyPath;
+  if (!privateKey && !privateKeyPath) {
+    logger.warn('【微信小程序】缺少 privateKey');
+    isValid = false;
+  }
+  if (!privateKey && privateKeyPath && !existsSync(privateKeyPath)) {
+    logger.warn('【微信小程序】privateKeyPath 没有对应文件');
+    isValid = false;
+  }
+  /* version */
+  const version = config?.['mp-weixin']?.version;
+  if (!version) {
+    logger.warn('【微信小程序】缺少 version');
+    isValid = false;
+  }
+  return isValid;
 };
 
 export const mpWeixinGetProject = (config: UniDeployConfig) =>
   new ci.Project({
-    ...config?.['mp-weixin']?.project,
-    appid: mpWeixinGetProjectAppid(config),
-    projectPath: mpWeixinGetProjectPath(config),
-    privateKeyPath: mpWeixinGetProjectPrivateKeyPath(config),
-    type: mpWeixinGetProjectType(config),
-  });
-
-export const mpWeixinGetUploadVersion = (config: UniDeployConfig) =>
-  (config?.['mp-weixin']?.upload?.version ??
-    getFileField(config, [
-      { entry: 'package.json', prop: 'version' },
-      { entry: ['src', 'manifest.json'], prop: ['versionName'] },
-      { entry: ['**', 'manifest.json'], prop: ['versionName'] },
-    ])) as string;
-
-export const mpWeixinGetUploadSetting = (config: UniDeployConfig) =>
-  (config?.['mp-weixin']?.upload?.setting ??
-    getFileField(config, [
-      { entry: ['dist', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-      { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-      { entry: ['dist', '**', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-    ])) as MiniProgramCI.ICompileSettings;
-
-export const mpWeixinGetUploadDesc = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.upload?.desc ?? 'Uploaded by uni-deploy';
+    ...config?.['mp-weixin'],
+  } as WechatCreateProjectOptions);
 
 export const mpWeixinUpload = async (config: UniDeployConfig, pRetryOptions?: PRetryOptions) =>
   pRetry(
     () =>
       ci.upload({
-        version: mpWeixinGetUploadVersion(config),
-        setting: mpWeixinGetUploadSetting(config),
-        desc: mpWeixinGetUploadDesc(config),
-        ...config?.['mp-weixin']?.upload,
+        ...config?.['mp-weixin'],
         project: mpWeixinGetProject(config),
-      }),
+      } as WechatUploadOptions),
     pRetryOptions,
   ) as Promise<WechatUploadResult>;
-
-export const mpWeixinGetPreviewVersion = (config: UniDeployConfig) =>
-  (config?.['mp-weixin']?.preview?.version ??
-    getFileField(config, [
-      { entry: 'package.json', prop: 'version' },
-      { entry: ['src', 'manifest.json'], prop: ['versionName'] },
-      { entry: ['**', 'manifest.json'], prop: ['versionName'] },
-    ])) as string;
-
-export const mpWeixinGetPreviewSetting = (config: UniDeployConfig) =>
-  (config?.['mp-weixin']?.preview?.setting ??
-    getFileField(config, [
-      { entry: ['dist', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-      { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-      { entry: ['dist', '**', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
-    ])) as MiniProgramCI.ICompileSettings;
-
-export const mpWeixinGetPreviewDesc = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.preview?.desc ?? 'Uploaded by uni-deploy';
-
-export const mpWeixinGetPreviewQrcodeFormat = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.preview?.qrcodeFormat ?? 'image';
-
-export const mpWeixinGetPreviewQrcodeOutputDest = (config: UniDeployConfig) =>
-  config?.['mp-weixin']?.preview?.qrcodeOutputDest ?? 'qrcode.png';
 
 export const mpWeixinPreview = async (config: UniDeployConfig, pRetryOptions?: PRetryOptions) =>
   pRetry(
     () =>
       ci.preview({
-        version: mpWeixinGetPreviewVersion(config),
-        setting: mpWeixinGetPreviewSetting(config),
-        desc: mpWeixinGetPreviewDesc(config),
-        qrcodeFormat: mpWeixinGetPreviewQrcodeFormat(config),
-        qrcodeOutputDest: mpWeixinGetPreviewQrcodeOutputDest(config),
-        ...config?.['mp-weixin']?.preview,
+        ...config?.['mp-weixin'],
         project: mpWeixinGetProject(config),
-      }),
+      } as WechatUploadOptions & { test?: true }),
     pRetryOptions,
   );
