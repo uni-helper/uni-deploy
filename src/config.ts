24443a3d8b265d0900d5eb881e337eef6882a949
ModyQyW <wurui-dev@foxmail.com>
2022-12-29 10:46:40 +0800

    feat: update config

--- src/config.ts
+++ src/config.ts
@@ -1,60 +1,127 @@
-import JoyCon from 'joycon';
-import { bundleRequire } from 'bundle-require';
-import stripJsonComments from 'strip-json-comments';
-import { loadJson } from './utils';
-import type { JoyConOptions, UniDeployConfig, UniDeployUserConfig } from './types';
-
-export const defaultCwd = process.cwd();
+import { loadConfig as _loadConfig } from 'unconfig';
+import { sourcePackageJsonFields } from 'unconfig/presets';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import { getFileDir, getFileField, getVersionField } from './utils';
+import type { UniDeployConfig, UniDeployUserConfig } from './types';
+import type { MiniProgramCI as Wechat } from 'miniprogram-ci/dist/@types/types';
 
 export const defaultConfig: UniDeployConfig = {
-  cwd: defaultCwd,
+  cwd: process.cwd(),
 };
 
 export const defineConfig = (config: UniDeployUserConfig) => config;
 
-export const mergeConfig = (config: UniDeployUserConfig) => ({
-  ...defaultConfig,
-  ...config,
-});
-
-export const loadConfig = async (
-  options?: Partial<JoyConOptions>,
-): Promise<{
-  path?: string;
-  data?: UniDeployConfig;
-}> => {
-  const joycon = new JoyCon();
-  const configPath = await joycon.resolve({
-    files: [
-      'uni-deploy.config.ts',
-      'uni-deploy.config.cts',
-      'uni-deploy.config.mts',
-      'uni-deploy.config.js',
-      'uni-deploy.config.cjs',
-      'uni-deploy.config.mjs',
-      'uni-deploy.config.json',
-      'package.json',
-    ],
-    packageKey: 'uni-deploy',
-    parseJSON: (json) => stripJsonComments(json),
-    ...options,
-  });
-
-  if (!configPath) return {};
-
-  if (configPath.endsWith('.json')) {
-    let data = await loadJson(configPath);
-    if (configPath.endsWith('package.json')) {
-      data = data?.['uni-deploy'];
-    }
-    return data ? { path: configPath, data } : {};
-  }
-
-  const config = await bundleRequire({
-    filepath: configPath,
-  });
-  return {
-    path: configPath,
-    data: config.mod['uni-deploy'] || config.mod.default || config.mod,
+export const loadEnvConfig = () => {
+  dotenvExpand.expand(dotenv.config());
+  const envConfig = {
+    'mp-weixin': {
+      privateKey: process.env.MP_WEIXIN_PRIVATE_KEY,
+      privateKeyPath: process.env.MP_WEIXIN_PRIVATE_KEY_PATH,
+    },
+    'mp-alipay': {
+      privateKey: process.env.MP_ALIPAY_PRIVATE_KEY,
+      toolId: process.env.MP_ALIPAY_TOOL_ID,
+    },
+    dingtalk: {
+      webhook: process.env.DINGTALK_WEBHOOK,
+    },
+    wecom: {
+      webhook: process.env.WECOM_WEBHOOK,
+    },
   };
+  return JSON.parse(JSON.stringify(envConfig)) as UniDeployUserConfig;
+};
+
+export const loadConfig = async (inlineConfig: UniDeployUserConfig = {}, cwd = process.cwd()) => {
+  const envConfig = loadEnvConfig();
+  const { config: loadedConfig = {} } = await _loadConfig<UniDeployUserConfig>({
+    cwd,
+    sources: [{ files: 'uni-deploy.config' }, sourcePackageJsonFields({ fields: 'uni-deploy' })],
+    merge: true,
+  });
+  const resolved: UniDeployConfig = {
+    ...defaultConfig,
+    ...envConfig,
+    ...loadedConfig,
+    ...inlineConfig,
+    'mp-weixin': {
+      appid: getFileField(
+        [
+          { entry: ['dist', 'mp-weixin', 'project.config.json'], prop: 'appid' },
+          { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'], prop: 'appid' },
+          { entry: ['dist', '**', 'mp-weixin', 'project.config.json'], prop: 'appid' },
+          { entry: ['src', 'project.config.json'], prop: 'appid' },
+          { entry: ['**', 'project.config.json'], prop: 'appid' },
+          { entry: ['src', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
+          { entry: ['**', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
+        ],
+        cwd,
+      ) as string | undefined,
+      projectPath: getFileDir(
+        [
+          { entry: ['dist', 'mp-weixin', 'project.config.json'] },
+          { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'] },
+          { entry: ['dist', '**', 'mp-weixin', 'project.config.json'] },
+          { entry: ['mp-weixin', 'project.config.json'] },
+          { entry: ['**', 'project.config.json'] },
+        ],
+        cwd,
+      ),
+      type: 'miniProgram',
+      version: getVersionField(cwd),
+      setting: getFileField([
+        { entry: ['dist', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
+        { entry: ['dist', 'build', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
+        { entry: ['dist', '**', 'mp-weixin', 'project.config.json'], prop: ['setting'] },
+      ]) as Wechat.ICompileSettings | undefined,
+      desc: 'Handled by uni-deploy',
+      qrcodeFormat: 'image',
+      qrcodeOutputDest: 'qrcode.png',
+      ...envConfig['mp-weixin'],
+      ...loadedConfig['mp-weixin'],
+      ...inlineConfig['mp-weixin'],
+    },
+    'mp-alipay': {
+      appid: getFileField(
+        [
+          { entry: ['dist', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
+          { entry: ['dist', 'build', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
+          { entry: ['dist', '**', 'mp-alipay', 'mini.project.json'], prop: 'appid' },
+          { entry: ['src', 'mini.project.json'], prop: 'appid' },
+          { entry: ['**', 'mini.project.json'], prop: 'appid' },
+          { entry: ['src', 'manifest.json'], prop: ['mp-alipay', 'appid'] },
+          { entry: ['**', 'manifest.json'], prop: ['mp-alipay', 'appid'] },
+        ],
+        cwd,
+      ) as string,
+      projectPath: getFileDir(
+        [
+          { entry: ['dist', 'mp-alipay', 'mini.project.json'] },
+          { entry: ['dist', 'build', 'mp-alipay', 'mini.project.json'] },
+          { entry: ['dist', '**', 'mp-alipay', 'mini.project.json'] },
+          { entry: ['mp-alipay', 'mini.project.json'] },
+          { entry: ['**', 'mini.project.json'] },
+        ],
+        cwd,
+      ),
+      version: getVersionField(cwd),
+      appxv: 'v2',
+      ...envConfig['mp-alipay'],
+      ...loadedConfig['mp-alipay'],
+      ...inlineConfig['mp-alipay'],
+    },
+    dingtalk: {
+      ...envConfig.dingtalk,
+      ...loadedConfig.dingtalk,
+      ...inlineConfig.dingtalk,
+    },
+    wecom: {
+      ...envConfig.wecom,
+      ...loadedConfig.wecom,
+      ...inlineConfig.wecom,
+    },
+  };
+
+  return JSON.parse(JSON.stringify(resolved));
 };
