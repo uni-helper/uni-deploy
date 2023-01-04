70035561a08f7c9d88fa53d831fffc0fc0137796
ModyQyW <wurui-dev@foxmail.com>
2023-01-04 14:58:11 +0800

    chore: update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'rollup';
 import { rollupIndexConfig, rollupIndexTypesConfig, rollupCliConfig } from '@modyqyw/utils';
 
-export default defineConfig([rollupIndexConfig(), rollupIndexTypesConfig(), rollupCliConfig()]);
+export default defineConfig([
+  rollupIndexConfig(undefined, undefined, false),
+  rollupIndexTypesConfig(),
+  rollupCliConfig(),
+]);
